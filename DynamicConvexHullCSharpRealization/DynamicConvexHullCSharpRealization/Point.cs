9cf1ca37937bcdb01ca3ec11cd6d0f717198cfd1
Andrey <andry@logvin.net>
2017-11-08 19:44:09 +0200

    Started realization of red black tree

---
+++ DynamicConvexHullCSharpRealization/DynamicConvexHullCSharpRealization/Point.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicConvexHullCSharpRealization
+{
+    class Point : IComparable<Point>
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int CompareTo(Point other)
+        {
+            if (this.Y < other.Y || (this.Y == other.Y && this.X < other.X))
+            {
+                return -1;
+            }
+            if (this.Y > other.Y || (this.Y == other.Y && this.X > other.X))
+            {
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
---
+++ DynamicConvexHullCSharpRealization/DynamicConvexHullCSharpRealization/RedBlackNode.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicConvexHullCSharpRealization
+{
+    class RedBlackNode
+    {
+        private RedBlackNode left;
+        private RedBlackNode right;
+        private RedBlackNode parent;
+
+        private bool isBlack;
+
+        private Point point;
+        private Bridge bridge;
+
+        private bool isLeaf { get { return left == null && right == null; } }
+        private bool isRoot { get { return parent == null; } }
+
+        private RedBlackNode grandparent { get { return parent?.parent; } }
+        private RedBlackNode uncle
+        {
+            get
+            {
+                if (grandparent == null)
+                {
+                    return null;
+                }
+                if (grandparent.left == parent)
+                {
+                    return grandparent.right;
+                }
+                return grandparent.left;
+            }
+        }
+
+        public RedBlackNode(Point newPoint = null, Bridge newBridge = null,
+                            RedBlackNode newParent = null, RedBlackNode newLeft = null, RedBlackNode newRight = null)
+        {
+            left = newLeft;
+            right = newRight;
+            parent = newParent;
+
+            point = newPoint;
+            bridge = newBridge;
+
+            isBlack = isRoot;
+        }
+
+        private void rotateLeft()
+        {
+            if (right != null)
+            {
+                RedBlackNode rightSon = right;
+                rightSon.parent = parent;
+
+                if (parent != null)
+                {
+                    if (parent.left == this)
+                    {
+                        parent.left = rightSon;
+                    }
+                    else
+                    {
+                        parent.right = rightSon;
+                    }
+                }
+                right = rightSon.left;
+                if (rightSon.left != null)
+                {
+                    rightSon.left.parent = this;
+                }
+                rightSon.left = this;
+                parent = rightSon;
+            }
+        }
+
+        private void rotateRight()
+        {
+            if (left != null)
+            {
+                RedBlackNode leftSon = left;
+                leftSon.parent = parent;
+
+                if (parent != null)
+                {
+                    if (parent.left == this)
+                    {
+                        parent.left = leftSon;
+                    }
+                    else
+                    {
+                        parent.right = leftSon;
+                    }
+                }
+                left = leftSon.right;
+                if (leftSon.right != null)
+                {
+                    leftSon.right.parent = this;
+                }
+                leftSon.right = this;
+                parent = leftSon;
+            }
+        }
+
+        public void
+    }
+}
