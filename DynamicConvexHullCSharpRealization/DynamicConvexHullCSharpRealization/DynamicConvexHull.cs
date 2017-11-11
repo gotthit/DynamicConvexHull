2a9282ae9166379f836708dd24dfe41a2029239f
Andrey <andry@logvin.net>
2017-11-11 23:17:42 +0200

    Implemented insertion

--- DynamicConvexHullCSharpRealization/DynamicConvexHullCSharpRealization/DynamicConvexHull.cs
+++ DynamicConvexHullCSharpRealization/DynamicConvexHullCSharpRealization/DynamicConvexHull.cs
@@ -42,6 +42,21 @@ namespace DynamicConvexHullCSharpRealization
                     return Grandparent.Left;
                 }
             }
+            public RedBlackNode Brother
+            {
+                get
+                {
+                    if (Parent == null)
+                    {
+                        return null;
+                    }
+                    if (Parent.Left == this)
+                    {
+                        return Parent.Right;
+                    }
+                    return Parent.Left;
+                }
+            }
 
             private RedBlackNode(Point newPoint = null, RedBlackNode newLeft = null, RedBlackNode newRight = null, RedBlackNode newParent = null)
             {
@@ -69,7 +84,7 @@ namespace DynamicConvexHullCSharpRealization
                 ConvexHull = Treap<Point>.Merge(newLeft?.ConvexHull, newRight?.ConvexHull);
             }
 
-            private void rotateLeft()
+            public void RotateLeft()
             {
                 if (Right != null)
                 {
@@ -97,7 +112,7 @@ namespace DynamicConvexHullCSharpRealization
                 }
             }
 
-            private void rotateRight()
+            public void RotateRight()
             {
                 if (Left != null)
                 {
@@ -141,17 +156,127 @@ namespace DynamicConvexHullCSharpRealization
             }
         }
 
-        private void insert(RedBlackNode current, Point pointToInsert)
+        public void Delete(Point pointToDelete)
         {
-            if (current.IsLeaf)
+            // we will delete only leafs
+        }
+
+        private static void recount(RedBlackNode current)
+        {
+            current.MaxPoint = Utils.Max(current.Left.MaxPoint, current.Right.MaxPoint);
+
+
+            throw new NotImplementedException();
+        }
+
+        private static void recountToUp(RedBlackNode current)
+        {
+            while (current != null)
             {
-                
+                recount(current);
+                current = current.Parent;
             }
         }
 
-        private void repair(RedBlackNode current)
+        private static void insert(RedBlackNode current, Point pointToInsert)
         {
+            if (current.IsLeaf)
+            {
+                if (pointToInsert.CompareTo(current.MaxPoint) <= 0)
+                {
+                    current.Left = new RedBlackNode(pointToInsert, current);
+                    current.Right = new RedBlackNode(current.MaxPoint, current);
+                }
+                else
+                {
+                    current.Left = new RedBlackNode(current.MaxPoint, current);
+                    current.Right = new RedBlackNode(pointToInsert, current);
+                }
+                recount(current);
+                initialInsertRepair(current);
+            }
+            else
+            {
+                if (pointToInsert.CompareTo(current.Left.MaxPoint) <= 0)
+                {
+                    insert(current.Left, pointToInsert);
+                }
+                else
+                {
+                    insert(current.Right, pointToInsert);
+                }
+            }
+        }
 
+        private static void initialInsertRepair(RedBlackNode current)
+        {
+            // considering the spacific of our insertion - if current not black (in this case we have to do nothing)
+            // current and current.Brother were red leafs before last insert, so we can make start already from 
+            // case 3 in https://ru.wikipedia.org/wiki/%D0%9A%D1%80%D0%B0%D1%81%D0%BD%D0%BE-%D1%87%D1%91%D1%80%D0%BD%D0%BE%D0%B5_%D0%B4%D0%B5%D1%80%D0%B5%D0%B2%D0%BE 
+
+            if (!current.IsBlack)
+            {
+                current.IsBlack = true;
+                current.Brother.IsBlack = true;
+                current.Parent.IsBlack = false;
+                repair(current.Parent);
+            }
+        }
+
+        private static void repair(RedBlackNode current)
+        {
+            recount(current);
+            if (current.IsRoot)
+            {
+                current.IsBlack = true;
+            }
+            else if (!current.Parent.IsBlack)
+            {
+                if (current.Uncle != null && !current.Uncle.IsBlack)
+                {
+                    current.Parent.IsBlack = true;
+                    current.Uncle.IsBlack = true;
+                    current.Grandparent.IsBlack = false;
+
+                    recount(current.Parent);
+
+                    repair(current.Grandparent);
+                }
+                else
+                {
+                    if (current.Grandparent.Left == current.Parent && current.Parent.Right == current)
+                    {
+                        current.Parent.RotateLeft();
+                        current = current.Left;
+
+                        recount(current);
+                    }
+                    else if (current.Grandparent.Right == current.Parent && current.Parent.Left == current)
+                    {
+                        current.Parent.RotateRight();
+                        current = current.Right;
+
+                        recount(current);
+                    }
+
+                    current.Parent.IsBlack = true;
+                    current.Grandparent.IsBlack = false;
+
+                    recount(current.Parent);
+
+                    if (current == current.Parent.Left)
+                    {
+                        current.Grandparent.RotateRight();
+                        recount(current.Right);
+                    }
+                    else
+                    {
+                        current.Grandparent.RotateLeft();
+                        recount(current.Left);
+                    }
+                    recountToUp(current);
+                }
+            }
         }
     }
 }
---
+++ DynamicConvexHullCSharpRealization/DynamicConvexHullCSharpRealization/Utils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicConvexHullCSharpRealization
+{
+    static class Utils
+    {
+        public static T Max<T>(T first, T second) where T : IComparable<T>
+        {
+            if (first.CompareTo(second) >= 0)
+            {
+                return first;
+            }
+            else
+            {
+                return second;
+            }
+        }
+    }
+}
