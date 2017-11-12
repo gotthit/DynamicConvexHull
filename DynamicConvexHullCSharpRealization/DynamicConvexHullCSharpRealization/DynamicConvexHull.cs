b15a9626afe07b0cadbcc28117dd76b9f0c8591c
Andrey <andry@logvin.net>
2017-11-12 19:03:27 +0200

    Implemented delete function

--- DynamicConvexHullCSharpRealization/DynamicConvexHullCSharpRealization/DynamicConvexHull.cs
+++ DynamicConvexHullCSharpRealization/DynamicConvexHullCSharpRealization/DynamicConvexHull.cs
@@ -152,13 +152,54 @@ namespace DynamicConvexHullCSharpRealization
             }
             else
             {
-                insert(leftHullRoot, pointToInsert);
+                leftHullRoot = insert(leftHullRoot, pointToInsert);
+            }
+
+            if (rightHullRoot == null)
+            {
+                rightHullRoot = new RedBlackNode(pointToInsert);
+            }
+            else
+            {
+                rightHullRoot = insert(rightHullRoot, pointToInsert);
             }
         }
 
         public void Delete(Point pointToDelete)
         {
-            // we will delete only leafs
+            if (leftHullRoot != null)
+            {
+                if (leftHullRoot.IsLeaf && leftHullRoot.MaxPoint.CompareTo(pointToDelete) == 0)
+                {
+                    leftHullRoot = null;
+                }
+                else
+                {
+                    RedBlackNode newRoot = delete(leftHullRoot, pointToDelete);
+                    // null means we deleted nothing
+                    if (newRoot != null)
+                    {
+                        leftHullRoot = newRoot;
+                    }
+                }
+            }
+
+            if (rightHullRoot != null)
+            {
+                if (rightHullRoot.IsLeaf && rightHullRoot.MaxPoint.CompareTo(pointToDelete) == 0)
+                {
+                    rightHullRoot = null;
+                }
+                else
+                {
+                    RedBlackNode newRoot = delete(rightHullRoot, pointToDelete);
+                    // null means we deleted nothing
+                    if (newRoot != null)
+                    {
+                        rightHullRoot = newRoot;
+                    }
+                }
+            }
         }
 
         private static void recount(RedBlackNode current)
@@ -169,16 +210,18 @@ namespace DynamicConvexHullCSharpRealization
             throw new NotImplementedException();
         }
 
-        private static void recountToUp(RedBlackNode current)
+        private static RedBlackNode recountToUp(RedBlackNode current)
         {
-            while (current != null)
+            while (!current.IsRoot)
             {
                 recount(current);
                 current = current.Parent;
             }
+            recount(current);
+            return current;
         }
 
-        private static void insert(RedBlackNode current, Point pointToInsert)
+        private static RedBlackNode insert(RedBlackNode current, Point pointToInsert)
         {
             if (current.IsLeaf)
             {
@@ -193,46 +236,51 @@ namespace DynamicConvexHullCSharpRealization
                     current.Right = new RedBlackNode(pointToInsert, current);
                 }
                 recount(current);
-                initialInsertRepair(current);
+                return initialInsertRepair(current);
             }
             else
             {
                 if (pointToInsert.CompareTo(current.Left.MaxPoint) <= 0)
                 {
-                    insert(current.Left, pointToInsert);
+                    return insert(current.Left, pointToInsert);
                 }
                 else
                 {
-                    insert(current.Right, pointToInsert);
+                    return insert(current.Right, pointToInsert);
                 }
             }
         }
 
-        private static void initialInsertRepair(RedBlackNode current)
+        private static RedBlackNode initialInsertRepair(RedBlackNode current)
         {
             // considering the spacific of our insertion - if current not black (in this case we have to do nothing)
             // current and current.Brother were red leafs before last insert, so we can make start already from 
             // case 3 in https://ru.wikipedia.org/wiki/%D0%9A%D1%80%D0%B0%D1%81%D0%BD%D0%BE-%D1%87%D1%91%D1%80%D0%BD%D0%BE%D0%B5_%D0%B4%D0%B5%D1%80%D0%B5%D0%B2%D0%BE 
 
-            if (!current.IsBlack)
+            if (current.IsBlack == false)
             {
                 current.IsBlack = true;
                 current.Brother.IsBlack = true;
                 current.Parent.IsBlack = false;
-                repair(current.Parent);
+                return repairAfterInsert(current.Parent);
+            }
+            else
+            {
+                return recountToUp(current);
             }
         }
 
-        private static void repair(RedBlackNode current)
+        private static RedBlackNode repairAfterInsert(RedBlackNode current)
         {
             recount(current);
             if (current.IsRoot)
             {
                 current.IsBlack = true;
+                return current;
             }
-            else if (!current.Parent.IsBlack)
+            else if (current.Parent.IsBlack == false)
             {
-                if (current.Uncle != null && !current.Uncle.IsBlack)
+                if (current.Uncle != null && current.Uncle.IsBlack == false)
                 {
                     current.Parent.IsBlack = true;
                     current.Uncle.IsBlack = true;
@@ -240,7 +288,7 @@ namespace DynamicConvexHullCSharpRealization
 
                     recount(current.Parent);
 
-                    repair(current.Grandparent);
+                    return repairAfterInsert(current.Grandparent);
                 }
                 else
                 {
@@ -274,7 +322,126 @@ namespace DynamicConvexHullCSharpRealization
                         current.Grandparent.RotateLeft();
                         recount(current.Left);
                     }
-                    recountToUp(current);
+                }
+            }
+            return recountToUp(current);
+        }
+
+        private static RedBlackNode delete(RedBlackNode current, Point pointToDelete)
+        {
+            // we will delete only leafs
+            // every node have two children
+
+            if (current.IsLeaf && current.MaxPoint.CompareTo(pointToDelete) == 0)
+            {
+                if (current.Grandparent != null)
+                {
+                    if (current.Grandparent.Left == current.Parent)
+                    {
+                        current.Grandparent.Left = current.Brother;
+                    }
+                    else
+                    {
+                        current.Grandparent.Right = current.Brother;
+                    }
+                }
+                current.Brother.Parent = current.Grandparent;
+
+                if (current.Brother.IsBlack == false || current.Parent.IsBlack == false)
+                {
+                    current.Brother.IsBlack = true;
+
+                    return recountToUp(current.Brother);
+                }
+                else
+                {
+                    return repairAfterDelete(current.Brother);
+                }
+            }
+            else if (!current.IsLeaf)
+            {
+                if (pointToDelete.CompareTo(current.Left.MaxPoint) <= 0)
+                {
+                    return delete(current.Left, pointToDelete);
+                }
+                else
+                {
+                    return delete(current.Right, pointToDelete);
+                }
+            }
+            // it means we deleted nothing
+            return null;
+        }
+
+        private static RedBlackNode repairAfterDelete(RedBlackNode current)
+        {
+            recount(current);
+            if (current.IsRoot)
+            {
+                return current;
+            }
+            else
+            {
+                if (current.Brother.IsBlack == false)
+                {
+                    current.Parent.IsBlack = false;
+                    current.Brother.IsBlack = true;
+                    if (current.Parent.Left == current)
+                    {
+                        current.Parent.RotateLeft();
+                    }
+                    else
+                    {
+                        current.Parent.RotateRight();
+                    }
+                }
+
+                if (current.Parent.IsBlack && current.Brother.IsBlack && current.Brother.Left.IsBlack && current.Brother.Right.IsBlack)
+                {
+                    current.Brother.IsBlack = false;
+                    return repairAfterDelete(current.Parent);
+                }
+                else if (current.Parent.IsBlack == false && current.Brother.IsBlack && current.Brother.Left.IsBlack && current.Brother.Right.IsBlack)
+                {
+                    current.Brother.IsBlack = false;
+                    current.Parent.IsBlack = true;
+                    return recountToUp(current.Parent);
+                }
+                else // if (current.Brother.IsBlack) always true
+                {
+                    if (current.Parent.Left == current && current.Brother.Left.IsBlack == false && current.Brother.Right.IsBlack)
+                    {
+                        current.Brother.IsBlack = false;
+                        current.Brother.Left.IsBlack = true;
+                        current.Brother.RotateRight();
+
+                        recount(current.Brother.Right);
+                        recount(current.Brother);
+                    }
+                    else if (current.Parent.Right == current && current.Brother.Left.IsBlack && current.Brother.Right.IsBlack == false)
+                    {
+                        current.Brother.IsBlack = false;
+                        current.Brother.Right.IsBlack = true;
+                        current.Brother.RotateLeft();
+
+                        recount(current.Brother.Left);
+                        recount(current.Brother);
+                    }
+
+                    current.Brother.IsBlack = current.Parent.IsBlack;
+                    current.Parent.IsBlack = true;
+
+                    if (current == current.Parent.Left)
+                    {
+                        current.Brother.Right.IsBlack = true;
+                        current.Parent.RotateRight();
+                    }
+                    else
+                    {
+                        current.Brother.Left.IsBlack = true;
+                        current.Parent.RotateLeft();
+                    }
+                    return recountToUp(current.Parent);
                 }
             }
         }
