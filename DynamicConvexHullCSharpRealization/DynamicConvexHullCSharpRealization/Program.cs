ea4dd9211362e0fc0995dfec50e935283271dd1e
Andrey <andry@logvin.net>
2017-11-15 08:47:06 +0200

    -

--- DynamicConvexHullCSharpRealization/DynamicConvexHullCSharpRealization/Program.cs
+++ DynamicConvexHullCSharpRealization/DynamicConvexHullCSharpRealization/Program.cs
@@ -16,41 +16,41 @@ namespace DynamicConvexHullCSharpRealization
 
             //hull.Delete(new Point(, ));
 
-            hull.Insert(new Point(4, 2));
+            //hull.Insert(new Point(4, 2));
 
-            hull.Insert(new Point(2, 4));
+            //hull.Insert(new Point(2, 4));
 
-            hull.Insert(new Point(1, 0));
+            //hull.Insert(new Point(1, 0));
 
-            hull.Insert(new Point(5, 5));
+            //hull.Insert(new Point(5, 5));
 
-            hull.Insert(new Point(0, 0));
+            //hull.Insert(new Point(0, 0));
 
-            hull.Insert(new Point(4, 4));
+            //hull.Insert(new Point(4, 4));
 
-            hull.Insert(new Point(3, 3));
+            //hull.Insert(new Point(3, 3));
 
-            hull.Insert(new Point(2, 2));
+            //hull.Insert(new Point(2, 2));
 
-            hull.Insert(new Point(3, 2));
+            //hull.Insert(new Point(3, 2));
 
-            hull.Insert(new Point(0, 1));
+            //hull.Insert(new Point(0, 1));
 
-            hull.Insert(new Point(1, 3));
+            //hull.Insert(new Point(1, 3));
 
-            hull.Insert(new Point(4, 1));
+            //hull.Insert(new Point(4, 1));
 
-            var rand = new Random(228);
+            //var rand = new Random(23523);
 
-            for (int i = 0; i < 50000; ++i)
-            {
-                hull.Insert(new Point(rand.Next(-1000000000, 1000000000), rand.Next(-1000000000, 1000000000)));
-            }
+            //for (int i = 0; i < 5000; ++i)
+            //{
+            //    hull.Insert(new Point(rand.Next(-1000000000, 1000000000), rand.Next(-1000000000, 1000000000)));
+            //}
 
-            for (int i = 0; i < 5000; ++i)
-            {
-                hull.Delete(new Point(rand.Next(-1000000000, 1000000000), rand.Next(-1000000000, 1000000000)));
-            }
+            //for (int i = 0; i < 500000; ++i)
+            //{
+            //    hull.Delete(new Point(rand.Next(-1000000000, 1000000000), rand.Next(-1000000000, 1000000000)));
+            //}
 
             //hull.Delete(new Point(1, 3));
             //hull.Delete(new Point(1, 3));
@@ -81,6 +81,16 @@ namespace DynamicConvexHullCSharpRealization
             //hull.Delete(new Point(3, 2));
 
             //hull.Delete(new Point(4, 2));
+
+
+
+            hull.Insert(new Point(4, 3));
+            hull.Insert(new Point(4, 2));
+            hull.Insert(new Point(6, 1));
+            hull.Insert(new Point(1, 3));
+            hull.Insert(new Point(6, 5));
+
+            hull.Delete(new Point(1, 3));
 
             hull.GetHull();
 
