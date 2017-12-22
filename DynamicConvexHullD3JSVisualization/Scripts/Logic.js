01a142c1bd6079112029552d4b082d3ce7ab200a
Andrey <andry@logvin.net>
2017-12-22 21:24:13 +0200

    temporary visualization

--- DynamicConvexHullD3JSVisualization/Scripts/Logic.js
+++ DynamicConvexHullD3JSVisualization/Scripts/Logic.js
@@ -10,14 +10,15 @@ $(document).ready(function () {
         let found = false;
 
         all_points.forEach(point => {
-            if (Math.sqrt((point.x - to_add.x) * (point.x - to_add.x) + (point.y - to_add.y) * (point.y - to_add.y)) <= 5) {                
+            if (Math.sqrt((point.x - to_add.x) * (point.x - to_add.x) + (point.y - to_add.y) * (point.y - to_add.y)) <= 14) {                
+                found = true;
+            }
+            if (Math.sqrt((point.x - to_add.x) * (point.x - to_add.x) + (point.y - to_add.y) * (point.y - to_add.y)) <= 10) {                
 
                 hull.delete_point(point);
 
                 point.x = -100;
                 point.y = -100;
-
-                found = true;
             }
         });
 
@@ -26,13 +27,22 @@ $(document).ready(function () {
             all_points.push(to_add);
         }
 
-        //alert("add " + (e.pageX ) +" "+ (-e.pageY ));   
-
         let canvas = document.getElementById("canvas");
         let ctx = canvas.getContext("2d");
-        ctx.clearRect(0, 0, 500, 500);
+        ctx.clearRect(0, 0, 1000, 1000);
 
         let list = hull.get_hull_as_array();
+
+
+        ctx.fillStyle = '#FFC373';
+        all_points.forEach(point => {
+            ctx.beginPath();
+            ctx.arc(point.x, -point.y, 7, 0, 2*Math.PI);
+            ctx.fill();
+        });
+
+        ctx.strokeStyle = '#4A2A82';
+        ctx.lineWidth = 2;
         ctx.beginPath();
         ctx.moveTo(list[list.length - 1].x, -list[list.length - 1].y);
         list.forEach(point => {
@@ -41,15 +51,14 @@ $(document).ready(function () {
         });
         ctx.stroke();
 
-        all_points.forEach(point => {
+        ctx.fillStyle = '#4A2A82';
+        list.forEach(point => {
             ctx.beginPath();
-            ctx.arc(point.x, -point.y, 5, 0, 2*Math.PI);
-            ctx.stroke();
+            ctx.arc(point.x, -point.y, 7, 0, 2*Math.PI);
+            ctx.fill();
         });
     });
 
-
-    
 
  });
 
